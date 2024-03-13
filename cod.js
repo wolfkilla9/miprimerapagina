0ec168bbc9f2f4df8131999275a56bb5724e0115
wolfkilla9 <mijaelgilperalta@gmail.com>
2024-03-12 20:05:53 -0500

    Initial commit

---
+++ cod.js
@@ -0,0 +1,22 @@
+/*alert("Hola este es mi Javascript");*/
+
+/*let nom1="ramon";
+nom1="julian";
+var nom2="ramon";
+
+const nom3="ramon";
+
+console.log(nom1);
+console.log(nom2);
+console.log(nom3);*/
+
+let nom = "TIENDA";
+
+const cambio = document.querySelector('.titulo');
+console.log(cambio);
+
+/*cam.innerHTML = nom;
+
+let texto = cam.innerText;*/
+
+
